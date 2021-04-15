08e1d8adca2ddfaf40f07e10e0685c6e037b1332
Jaimie Jin <jinjaimie12@gmail.com>
2021-04-14 17:31:46 -0700

    Money done

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 struct DomainModel {
     var text = "Hello, World!"
         // Leave this here; this value is also tested in the tests,
@@ -9,6 +11,58 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    var amount: Int
+    var currency: String
+    
+    init(amount a: Int, currency c: String) {
+        let acceptedCurrency = ["USD", "GBP", "EUR", "CAN"]
+        if (acceptedCurrency.contains(c)) {
+            amount = a
+            currency = c
+        } else {
+            amount = 0
+            currency = "USD"
+            print("currency not accepted")
+        }
+    }
+    
+    func convert(_ c: String) -> Money {
+        let exchangeRate = ["GBP": 0.5/2, "EUR": 1.5/2, "CAN": 1.25/4]
+        var USDamount = Double(amount)
+        if (self.currency != "USD") {
+            USDamount = Double(self.amount) / exchangeRate[self.currency]!
+        }
+        let newAmount = Int(round(USDamount * exchangeRate[c]!))
+        return Money(amount: newAmount, currency: c)
+    }
+    
+    func add(_ m: Money) -> Money {
+        let exchangeRate = ["GBP": 0.5/2, "EUR": 1.5/2, "CAN": 1.25/4]
+        var CUSDamount = Double(self.amount)
+        if (self.currency != "USD") {
+            CUSDamount = Double(self.amount) / exchangeRate[self.currency]!
+        }
+        var NUSDamount = Double(m.amount)
+        if (m.currency != "USD") {
+            NUSDamount = Double(m.amount) / exchangeRate[m.currency]!
+        }
+        let newAmount = Int(round((CUSDamount + NUSDamount) * exchangeRate[m.currency]!))
+        return Money(amount: newAmount, currency: m.currency)
+    }
+    
+    func subtract(_ m: Money) -> Money {
+        let exchangeRate = ["GBP": 0.5/2, "EUR": 1.5/2, "CAN": 1.25/4]
+        var CUSDamount = Double(self.amount)
+        if (self.currency != "USD") {
+            CUSDamount = Double(self.amount) / exchangeRate[self.currency]!
+        }
+        var NUSDamount = Double(m.amount)
+        if (m.currency != "USD") {
+            NUSDamount = Double(m.amount) / exchangeRate[m.currency]!
+        }
+        let newAmount = Int(round((CUSDamount - NUSDamount) * exchangeRate[m.currency]!))
+        return Money(amount: newAmount, currency: m.currency)
+    }
 }
 
 ////////////////////////////////////
